7210b7a932d8c9ae775586038f99d17559dd1add
mohamedeltahan1470 <mohamedeltahan1470@gmail.com>
2024-05-30 09:55:54 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let toastBox = document.getElementById('toastBox');
+let successMsg = ' <i class="fas fa-check-circle"></i> Successfully submitted';
+let errorMsg = '<i class="fas fa-times-circle"></i>  Please fix the error1';
+let InvalidMsg = '<i class="fas fa-exclamation-circle"></i>  Invalid input, check again';
+
+function showToast(msg){
+    let toast  = document.createElement('div');
+    toast.classList.add('toast');
+    toast.innerHTML = msg;
+    toastBox.appendChild(toast);
+    if(msg.includes('error')){
+        toast.classList.add('error');
+    }
+    if(msg.includes('Invalid')){
+        toast.classList.add('invalid');
+    }
+    setTimeout(()=>{
+        toast.remove();
+    },5000);
+}
+showToast;
